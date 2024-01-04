214d431b11ef4214e79c6f3a26df96cb916eeb91
calm428 <ollie21428@gmail.com>
2024-01-04 13:07:29 +0200

    created the upcoming component

--- app/blogs-and-news/page.tsx
+++ app/blogs-and-news/page.tsx
@@ -1,18 +1,21 @@
-import BlogsSection from '@/components/blogs-and-news/blogs';
-import HeroSection from '@/components/blogs-and-news/hero';
-import NewsSection from '@/components/blogs-and-news/news';
-import PaginationSection from '@/components/blogs-and-news/pagination';
-import React from 'react'
+import React from "react"
+
+import BlogsSection from "@/components/blogs-and-news/blogs"
+import HeroSection from "@/components/blogs-and-news/hero"
+import NewsSection from "@/components/blogs-and-news/news"
+import PaginationSection from "@/components/blogs-and-news/pagination"
+import UpcomingSection from "@/components/blogs-and-news/upcoming"
 
 const BlogAndNewsPage = () => {
-    return (
-        <div className='mb-56'>
-            <HeroSection />
-            <BlogsSection />
-            <NewsSection />
-            <PaginationSection />
-        </div>
-    );
-};
+  return (
+    <div className="mb-56">
+      <HeroSection />
+      <BlogsSection />
+      <NewsSection />
+      <UpcomingSection />
+      <PaginationSection />
+    </div>
+  )
+}
 
-export default BlogAndNewsPage;
+export default BlogAndNewsPage
