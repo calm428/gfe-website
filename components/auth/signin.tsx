736c702ff4872f90c88978ec2e533f499a2fa026
calm428 <ollie21428@gmail.com>
2024-01-04 13:10:36 +0200

    updated signin

--- components/auth/signin.tsx
+++ components/auth/signin.tsx
@@ -39,13 +39,17 @@ export function SignInModal({
             />
           </div>
           <div className="md:w-[45%] w-full p-8 sm:p-24 md:p-8 !py-24">
-            <div className="my-4">
-              <div className="text-3xl font-bold">Start Mining!</div>
+            <div className="my-4 text-center">
+              <div className="text-3xl font-bold ">Start Mining!</div>
               <div> Continue Your Journey</div>
             </div>
             <Button variant="outline" className="w-full mt-4">
               <Icons.google className="mr-2 h-4 w-4" />
               Continue with Google
+            </Button>
+            <Button variant="outline" className="w-full mt-4">
+              <Icons.apple className="mr-2 h-5 w-5" />
+              Continue with Apple
             </Button>
             <div className="flex items-center justify-center gap-2 relative my-1">
               <div className="p-4 bg-white z-10">OR</div>
@@ -54,7 +58,7 @@ export function SignInModal({
                 className="absolute w-full mr-auto h-[1px] border-0"
               />
             </div>
-            <div className="space-y-4 mb-12">
+            <div className="space-y-4 mb-4">
               <Input type="email" placeholder="Username or email" />
               <Input type="password" placeholder="Password" />
               <Button className="w-full">Sign In</Button>
