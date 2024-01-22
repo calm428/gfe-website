05fdbbdfbdc838c1f98f8de98460ca9a3b3842f6
Oliver Cawson <cawsonoliver@gmail.com>
2024-01-22 22:24:46 +0800

    fix: change favicon icon and title

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "GFE Foundation",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   token_functionalities: [
