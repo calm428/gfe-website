8cfb51a72d6aa902baabef021b8d69f137e5a4ee
Oliver Cawson <cawsonoliver@gmail.com>
2024-03-01 16:34:58 +0900

    feat: add correct guild position to google map

--- components/guild/guild-map.tsx
+++ components/guild/guild-map.tsx
@@ -8,13 +8,16 @@ import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 import Image from "next/image"
+import Link from "next/link"
 import logoIcon from "@/public/logo-small.png"
 import { Icon } from "leaflet"
 import { useMapEvents } from "react-leaflet"
 
+import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 
 import SectionTitle from "../common/section-title"
+import { Button } from "../ui/button"
 
 interface MapEventsProps {
   setPanelVisible: React.Dispatch<React.SetStateAction<boolean>>
@@ -45,7 +48,7 @@ export default function GuildMap() {
       </SectionTitle>
       <div className="container relative mt-6 w-full overflow-hidden">
         <MapContainer
-          center={[24.1422, 110.3108]}
+          center={[24.1422, -110.3108]}
           zoom={7.5}
           scrollWheelZoom={true}
           className="h-[500px] w-full"
@@ -55,7 +58,7 @@ export default function GuildMap() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <Marker
-            position={[24.1422, 110.3108]}
+            position={[24.1422, -110.3108]}
             icon={icon}
             eventHandlers={{
               click: () => {
@@ -67,10 +70,36 @@ export default function GuildMap() {
         </MapContainer>
         <div
           className={cn(
-            "absolute -left-1/2 top-0 z-[1000] h-full w-1/2 bg-gray-100 transition-all sm:-left-1/3 sm:w-1/3 lg:-left-1/4 lg:w-1/4",
+            "absolute -left-1/2 top-0 z-[1000] h-full w-1/2 space-y-2 bg-gray-100 p-4 transition-all sm:-left-1/3 sm:w-1/3 lg:-left-1/4 lg:w-1/4",
             { "!left-0": panelVisible }
           )}
-        ></div>
+        >
+          <div className="flex flex-col">
+            <span className="text-sm uppercase text-gray-500">
+              {t("guild.leader")}
+            </span>
+            <p>John Doe</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm uppercase text-gray-500">
+              {t("guild.capacity")}
+            </span>
+            <p>3500kWh/d</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm uppercase text-gray-500">
+              {t("guild.community_channel")}
+            </span>
+            <span className="text-xs uppercase text-gray-600">- Discord</span>
+            <p>
+              <Button variant="link">
+                <Link href={siteConfig.links.discord} target="_blank">
+                  {siteConfig.links.discord}
+                </Link>
+              </Button>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   )
