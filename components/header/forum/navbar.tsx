0723966d37e4a63e751790a02393ebca8e50b4ef
calm428 <ollie21428@gmail.com>
2024-10-12 10:54:27 +0200

    Fix bugs

--- components/header/forum/navbar.tsx
+++ components/header/forum/navbar.tsx
@@ -12,10 +12,9 @@ import {
 } from "@nextui-org/react"
 import { getServerSession } from "next-auth"
 
-import AvatarWithMenu from "./avatar-with-menu"
+import AvatarWithMenu from "../avatar-with-menu"
 import MobileMenu from "./mobile-menu"
-// import NavLinks from "./nav-links"
-import WalletConnectButton from "./wallet-connect-button"
+import WalletConnectButton from "../wallet-connect-button"
 
 async function getUser() {
   const session = await getServerSession(authOptions)
@@ -52,7 +51,6 @@ export async function Navbar() {
             />
           </NextLink>
         </NavbarBrand>
-        {/* <NavLinks /> */}
       </NavbarContent>
 
       <NavbarContent className="hidden basis-1 pl-4 lg:flex" justify="end">
